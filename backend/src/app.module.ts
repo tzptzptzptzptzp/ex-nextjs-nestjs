cc946bfde8c38a3d9fca49cf095534eb74dd66a9
tzptzptzptzptzp <tzp2539@gmail.com>
2023-08-04 23:47:45 +0900

    ✨ TypeORM設定ファイルを作成

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from './typeorm.config';
 
 @Module({
-  imports: [PostsModule],
+  imports: [TypeOrmModule.forRoot(config), PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/typeorm.config.ts
@@ -0,0 +1,15 @@
+require('dotenv').config();
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+const config: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: false,
+};
+
+export default config;
