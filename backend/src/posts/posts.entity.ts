538b6ea692373d534e24bc986ec49aeb0fe336fa
tzptzptzptzptzp <tzp2539@gmail.com>
2023-08-04 23:50:45 +0900

    ✨ エンティティファイルを作成

---
+++ backend/src/posts/posts.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Posts {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  published: number;
+}
