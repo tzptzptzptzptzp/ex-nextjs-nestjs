15a68585a910d90f864a7812a17cf36bdb92f8ab
tzptzptzptzptzp <tzp2539@gmail.com>
2023-08-05 01:48:54 +0900

    ✨ 全てのデータを取得するAPIを実装

--- backend/src/posts/posts.controller.ts
+++ backend/src/posts/posts.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { Post } from './post.entity';
 
 @Controller('posts')
 export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
   @Get()
-  findAll() {
-    return 'Get all posts';
+  findAll(): Promise<Post[]> {
+    return this.postsService.findAll();
   }
 }
--- backend/src/posts/posts.service.ts
+++ backend/src/posts/posts.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Post } from './post.entity';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    @InjectRepository(Post)
+    private postsRepository: Repository<Post>,
+  ) {}
+
+  async findAll(): Promise<Post[]> {
+    return this.postsRepository.find();
+  }
+}
