5aa1b8d78c1fe392e77acde16ed98935c6a7d5bb
tzptzptzptzptzp <tzp2539@gmail.com>
2023-08-07 08:00:11 +0900

    ✨ 取得したデータを表示

--- frontend/pages/post/[id].tsx
+++ frontend/pages/post/[id].tsx
@@ -1,7 +1,8 @@
-import Head from 'next/head'
 import React from 'react'
-import { PostType } from '../../src/types/PostType'
+import Head from 'next/head'
+import Link from 'next/link'
 import { GetServerSideProps } from 'next'
+import { PostType } from '../../src/types/PostType'
 
 interface PostPageProps {
   data: PostType
@@ -19,6 +20,14 @@ export const getServerSideProps: GetServerSideProps<PostPageProps> = async ({ pa
 }
 
 export default function Post({ data }: PostPageProps) {
+  const formattedDate = new Intl.DateTimeFormat('ja-JP', {
+    year: '2-digit',
+    month: '2-digit',
+    day: '2-digit',
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZone: 'Asia/Tokyo'
+  }).format(new Date(data.created_at))
   return (
     <div className='flex items-center justify-center h-screen'>
       <Head>
@@ -28,6 +37,18 @@ export default function Post({ data }: PostPageProps) {
       </Head>
 
       <main className='w-2/5'>
+        <div className='mb-6 px-8 py-4 rounded-xl' style={{ boxShadow: '0px 5px 15px -5px #b0b0b0' }}>
+          <h2 className='mb-8 text-2xl text-center'>{data.title}</h2>
+          <div className='flex items-center justify-between text-sm'>
+            <p>{formattedDate}</p>
+            <p>{data.published === 0 ? '非公開' : '公開'}</p>
+          </div>
+        </div>
+        <div className='w-full text-center'>
+          <Link href='/' className='text-xl hover:opacity-50 duration-300'>
+            - HOME -
+          </Link>
+        </div>
       </main>
     </div>
   )
