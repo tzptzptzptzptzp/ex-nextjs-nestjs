a5163c7714464e1083b3840617d10cadfe1d0dbc
tzptzptzptzptzp <tzp2539@gmail.com>
2023-08-10 01:00:39 +0900

    ✨ データを更新する機能を実装

--- frontend/pages/post/[id].tsx
+++ frontend/pages/post/[id].tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 import { GetServerSideProps } from 'next'
-import { PostType } from '../../src/types/PostType'
 import { DeleteBtn } from '../../src/components/deleteBtn'
+import { EditBtn } from '../../src/components/editBtn'
+import { PostType } from '../../src/types/PostType'
 
 interface PostPageProps {
   data: PostType
@@ -33,6 +34,53 @@ export const getServerSideProps: GetServerSideProps<PostPageProps> = async ({ pa
 }
 
 export default function Post({ data }: PostPageProps) {
+  const [title, setTitle] = useState('')
+  const [published, setPublished] = useState(1)
+  const [isEditing, setIsEditing] = useState(false)
+  const [isEmpty, setIsEmpty] = useState(false)
+  const [isSuccess, setIsSuccess] = useState('')
+
+  const handleEdit = () => {
+    if (!isEditing) {
+      setTitle(data.title)
+      setPublished(data.published)
+    }
+    setTimeout(function () {
+      setIsEditing(!isEditing)
+    }, 100)
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    console.log(data.id);
+
+    e.preventDefault()
+    if (isEditing) {
+      if (title) {
+        try {
+          const response = await fetch(`http://localhost:5000/posts/${data.id}`, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ title, published })
+          })
+          if (response.ok) {
+            setIsSuccess('success')
+            setIsEmpty(false)
+            setTitle('')
+            setPublished(1)
+          } else {
+            setIsSuccess('failed')
+          }
+        } catch (err) {
+          console.error('Error posting data:', err)
+        }
+      } else {
+        setIsEmpty(true)
+      }
+    }
+  }
+
   const formattedDate = new Intl.DateTimeFormat('ja-JP', {
     year: '2-digit',
     month: '2-digit',
@@ -41,6 +89,7 @@ export default function Post({ data }: PostPageProps) {
     minute: 'numeric',
     timeZone: 'Asia/Tokyo'
   }).format(new Date(data.created_at))
+
   return (
     <div className='flex items-center justify-center h-screen'>
       <Head>
@@ -50,14 +99,37 @@ export default function Post({ data }: PostPageProps) {
       </Head>
 
       <main className='w-2/5'>
-        <div className='relative mb-6 px-8 py-4 rounded-xl' style={{ boxShadow: '0px 5px 15px -5px #b0b0b0' }}>
-          <h2 className='mb-8 text-2xl text-center'>{data.title}</h2>
-          <div className='flex items-center justify-between text-sm'>
-            <p>{formattedDate}</p>
-            <p>{data.published === 0 ? '非公開' : '公開'}</p>
-          </div>
-          <DeleteBtn postId={data.id}></DeleteBtn>
-        </div>
+        {!isEditing ? (
+          <>
+            <div className='relative mb-6 px-8 py-4 rounded-xl' style={{ boxShadow: '0px 5px 15px -5px #b0b0b0' }}>
+              <h2 className='mb-8 text-2xl text-center'>{data.title}</h2>
+              <div className='flex items-center justify-between text-sm'>
+                <p>{formattedDate}</p>
+                <p>{data.published === 0 ? '非公開' : '公開'}</p>
+              </div>
+              <EditBtn handleEdit={handleEdit} handleSubmit={handleSubmit} isEditing={isEditing}></EditBtn>
+              <DeleteBtn postId={data.id}></DeleteBtn>
+            </div>
+          </>
+        ) : (
+          <>
+            <form onSubmit={handleSubmit} className='relative mb-6 px-8 py-4 rounded-xl' style={{ boxShadow: '0px 5px 15px -5px #b0b0b0' }}>
+              <div className='flex justify-center w-full mb-8'>
+                <input type="text" name="title" value={title} onChange={(e) => setTitle(e.target.value)} className='border w-4/5 mx-auto px-2 py-1' />
+              </div>
+
+              <div className='flex items-center justify-between text-sm'>
+                <p>{formattedDate}</p>
+                <select name="published" value={published} onChange={(e) => setPublished(parseInt(e.target.value, 10))} className='border py-1'>
+                  <option value={1}>公開</option>
+                  <option value={0}>非公開</option>
+                </select>
+              </div>
+              <EditBtn handleEdit={handleEdit} handleSubmit={handleSubmit} isEditing={isEditing}></EditBtn>
+              <DeleteBtn postId={data.id}></DeleteBtn>
+            </form>
+          </>
+        )}
         <div className='w-full text-center'>
           <Link href='/' className='text-xl hover:opacity-50 duration-300'>
             - HOME -
